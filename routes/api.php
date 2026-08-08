7b0f6278271ad871a52a195f80cfe6439fcc0ba0
RawalAnuma <rawalanuma@gmail.com>
2026-08-08 21:46:07 +0545

    feat: add food API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FoodController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('foods', FoodController::class);
