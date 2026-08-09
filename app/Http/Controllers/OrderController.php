9c68be9274f75f0a6bc5701140c83182b887f14e
RawalAnuma <rawalanuma@gmail.com>
2026-08-09 08:49:54 +0545

    feat: add order controller

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreOrderRequest;
+use App\Http\Requests\UpdateOrderStatusRequest;
+use App\Http\Resources\OrderResource;
+use App\Repositories\Contracts\OrderRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+
+class OrderController extends Controller
+{
+    public function __construct(
+        private OrderRepositoryInterface $orderRepository
+    ) {
+    }
+
+    public function index()
+    {
+        $orders = $this->orderRepository->getAll();
+
+        return OrderResource::collection($orders);
+    }
+
+    public function store(StoreOrderRequest $request)
+    {
+        $order = $this->orderRepository->create(
+            $request->validated()
+        );
+
+        return (new OrderResource($order))
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    public function show(int $id)
+    {
+        $order = $this->orderRepository->findById($id);
+
+        if (!$order) {
+            return response()->json([
+                'message' => 'Order not found.'
+            ], 404);
+        }
+
+        return new OrderResource($order);
+    }
+
+    public function updateStatus(
+        UpdateOrderStatusRequest $request,
+        int $id
+    ): OrderResource|JsonResponse {
+        $order = $this->orderRepository->findById($id);
+
+        if (!$order) {
+            return response()->json([
+                'message' => 'Order not found.'
+            ], 404);
+        }
+
+        $order = $this->orderRepository->updateStatus(
+            $order,
+            $request->validated('status')
+        );
+
+        return new OrderResource($order);
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $order = $this->orderRepository->findById($id);
+
+        if (!$order) {
+            return response()->json([
+                'message' => 'Order not found.'
+            ], 404);
+        }
+
+        $this->orderRepository->delete($order);
+
+        return response()->json([
+            'message' => 'Order deleted successfully.'
+        ]);
+    }
+}
