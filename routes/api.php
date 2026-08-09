6b8269bc453025419fff2a13a92eb7eb078d4d3f
RawalAnuma <rawalanuma@gmail.com>
2026-08-09 08:50:06 +0545

    feat: add order API routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +12,13 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('categories', CategoryController::class);
+
 Route::apiResource('foods', FoodController::class);
+
+Route::apiResource('orders', OrderController::class)
+    ->except(['update']);
+
+Route::patch(
+    'orders/{order}/status',
+    [OrderController::class, 'updateStatus']
+);
