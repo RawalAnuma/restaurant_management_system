91560f4ce67a6419249b1b7816e0e4ade5bd0f7f
RawalAnuma <rawalanuma@gmail.com>
2026-08-14 15:38:50 +0545

    chore: update database seeder configuration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,8 +9,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             CategorySeeder::class,
             FoodSeeder::class,
+            OrderSeeder::class,
         ]);
     }
 }
